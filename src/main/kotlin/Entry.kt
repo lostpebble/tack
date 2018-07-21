be90f6e7d2aed738e997dfc7c5a011c5c3da4744
Paul Myburgh <paulmyburgh88@gmail.com>
2018-07-21 20:04:10 +0200

    Figuring out which config / in-memory data storage to use

--- src/main/kotlin/Entry.kt
+++ src/main/kotlin/Entry.kt
@@ -1,6 +1,10 @@
 import java.nio.file.FileSystems
 import java.nio.file.Files
+import java.sql.Connection
+import java.sql.DriverManager
 
 fun main(args: Array<String>) {
     loadConfig()
+
+    val sqlConnection = DriverManager.getConnection("jdbc:h2:mem:tack;DB_CLOSE_DELAY=-1")
 }
